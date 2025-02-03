49793dc7ed90ce1f684c4f4e345937092569c322
Uzma Shaheen <ushaheen0000@gmail.com>
2025-02-03 08:42:56 +0500

    Updated Feature Products

--- src/components/productgrid.tsx
+++ src/components/productgrid.tsx
@@ -17,7 +17,7 @@ export const getProdctsData = async () => {
   }`);
   return res;
 };
-// *[_type=="product" && _id in["gbTY3B2TxF1zfilLOh7ZwD", "gbTY3B2TxF1zfilLOh7FG0"]]{
+
 interface IProduct {
   name: string;
   description: string;
@@ -31,8 +31,8 @@ export default async function ProductGrid() {
   const data: IProduct[] = await getProdctsData();
 
   return (
-    <div className="max-width max-w-6xl mx-auto px-4 py-8">
-      <h1 className="text-2xl font-bold mb-6 text-center">Featured Products</h1>
+    <div className="max-width max-w-7xl mx-auto px-4 py-8">
+      <h1 className="text-2xl sm:text-3xl lg:text-4xl font-bold mb-6 text-center">Featured Products</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {data.map((product) => (
           <div
@@ -53,10 +53,10 @@ export default async function ProductGrid() {
                     />
                   </div>
                 )}
-                <h2 className="text-lg font-semibold mt-4 text-[#FB2E86]">
+                <h2 className="text-lg sm:text-xl font-semibold mt-4 text-[#FB2E86]">
                   {product.name}
                 </h2>
-                <h3 className="text-[#151875] mt-2 justify-center items-center">
+                <h3 className="text-[#151875] mt-2 text-base sm:text-lg">
                   ${product.price}
                 </h3>
               </div>
